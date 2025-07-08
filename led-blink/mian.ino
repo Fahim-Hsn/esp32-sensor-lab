61de7446f7db165c004d0f624f0d8d14372d3e05
Fahim Hossain <158022413+Fahim-Hsn@users.noreply.github.com>
2025-07-09 01:31:41 +0600

    Update mian.ino

--- led-blink/mian.ino
+++ led-blink/mian.ino
@@ -1,1 +1,11 @@
+```cpp
+void setup() {
+  pinMode(2, OUTPUT); // GPIO 2 controls LED
+}
 
+void loop() {
+  digitalWrite(2, HIGH); // Turn ON LED
+  delay(1000);           // Wait 1 second
+  digitalWrite(2, LOW);  // Turn OFF LED
+  delay(1000);           // Wait 1 second
+}```
