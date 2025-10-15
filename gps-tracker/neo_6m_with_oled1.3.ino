b78985d65c49208f789ff28f8356b695e54e21a1
Fahim Hossain <158022413+Fahim-Hsn@users.noreply.github.com>
2025-10-15 23:24:36 +0600

    Create neo_6m_with_oled1.3.ino

---
+++ gps-tracker/neo_6m_with_oled1.3.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>
+#include <U8g2lib.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+TinyGPSPlus gps;
+
+HardwareSerial GPSserial(2);  
+
+unsigned long lastDisplayMillis = 0;
+
+void setup() {
+  Serial.begin(115200);  
+  GPSserial.begin(9600, SERIAL_8N1, 16, 17);  
+
+  u8g2.begin();
+  u8g2.clearBuffer();  
+  u8g2.setFont(u8g2_font_ncenB08_tr);  
+  u8g2.drawStr(0, 12, "Starting up...");
+  u8g2.sendBuffer(); 
+}
+
+void loop() {
+  while (GPSserial.available()) {  
+    gps.encode(GPSserial.read());  
+  }
+
+  unsigned long now = millis();
+
+
+  if (now - lastDisplayMillis > 1000) {
+    lastDisplayMillis = now;
+
+    u8g2.clearBuffer();  
+    u8g2.setFont(u8g2_font_ncenB08_tr);  
+
+    if (gps.location.isValid()) {
+      char latbuf[32], lonbuf[32], satbuf[16];
+
+      snprintf(latbuf, sizeof(latbuf), "Lat: %.6f", gps.location.lat());
+      snprintf(lonbuf, sizeof(lonbuf), "Lon: %.6f", gps.location.lng());
+      snprintf(satbuf, sizeof(satbuf), "Sats: %u", gps.satellites.value());
+
+      u8g2.drawStr(0, 12, latbuf);
+      u8g2.drawStr(0, 28, lonbuf);
+      u8g2.drawStr(0, 44, satbuf);
+    } else {
+      u8g2.drawStr(0, 24, "Waiting for Satelight...");
+    }
+
+    u8g2.sendBuffer();  
+  }
+}
